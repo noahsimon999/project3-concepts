58155f189a8f355cba83c5fd01b645cadd99724d
nohabroha <noahysimon@gmail.com>
2018-10-20 18:27:23 -0500

    tester

---
+++ app.js
@@ -0,0 +1,9 @@
+var elem = document.querySelector('.sidenav');
+var instance = M.Sidenav.init(elem, {
+    onOpenStart: function () {
+        console.log("I trigger as soon as the page is loaded");
+    },
+    onCloseEnd: function () {
+        console.log("same");
+    }
+});
